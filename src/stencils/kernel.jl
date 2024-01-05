6aa32677e3532165a309467d7ebd19d5a7726cd8
Alex Gardner <alex.s.gardner@jpl.nasa.gov>
2024-01-05 14:02:12 -0800

    Add example to Kernel and update ReadMe

--- src/stencils/kernel.jl
+++ src/stencils/kernel.jl
@@ -61,6 +61,33 @@ the stencil and kernel, like [`kernelproduct`](@ref) can be used in
 
 A function `f` may be passed as the first argument,
 and a kernel array will be calculated with `map(f, distances(stencil))`.
+
+As an example, Kernels can be convolved with an Array
+```julia
+using Stencils
+
+# Define a random array that the kernel will be convolved with
+r = rand(1000, 1000)
+
+# Define kernel array
+sharpen = [0 -1 0;
+           -1 5 -1;
+           0 -1 0]
+
+# Define a stencil that is the same size as the kernel array
+stencil = Window(1)
+
+# Create a stencil Kernel from the stencil and the kernel array
+k = Kernel(stencil, sharpen)
+
+# Wrap the random array and the Kernel in a StencilArray
+A = StencilArray(r, k)
+
+# use `mapstencil` with the `kernelproduct` funciton to convolve the Kernel with array
+# Note: Base.dot product could also be used as input to `mapstencil`, but `kernelproduct` 
+# lets you use an array of StaticArray and it will still work (dot is recursive)
+mapstencil(kernelproduct, A) 
+```
 """
 struct Kernel{R,N,L,T,F,H,K} <: AbstractKernelStencil{R,N,L,T,H}
     f::F
