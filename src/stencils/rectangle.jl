b1b652edc12ff0a7eb78e293df2c2b5f9c0f61e5
rafaqz <rafaelschouten@gmail.com>
2024-01-07 10:07:48 +0100

    fix docs

--- src/stencils/rectangle.jl
+++ src/stencils/rectangle.jl
@@ -6,25 +6,9 @@ const AxisOffsets = Tuple{Int,Int}
     Rectangle(offsets::Tuple{Tuple}...)
     Rectangle{O}()
 
-Stencils that can take arbitrary shapes by specifying each coordinate,
-as `Tuple{Int,Int}` of the row/column distance (positive and negative)
-from the central point.
-
-The stencil radius is calculated from the most distant coordinate,
-and the dimensionality `N` of the stencil is taken from the length of
-the first coordinate, e.g. `1`, `2` or `3`.
-
-See [`NamedStencil`](@ref) for a similar stencil with named offsets.
-
-## Example
-
-```julia
-julia> p = Rectangle((0, -1), (2, 1)) 
-Positional{((0, -1), (2, 1), (-1, 1), (0, 1)), 2, 2, 4, Nothing}
-   ▄ 
- ▀ ▀ 
-   ▀ 
-```
+Rectanglar stencils of arbitrary shapes. These are 
+specified with pulles of offsets around the center point, 
+one for each dimension.
 """
 struct Rectangle{O,R,N,L,T} <: Stencil{R,N,L,T}
     neighbors::SVector{L,T}
