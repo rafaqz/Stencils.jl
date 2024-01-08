098623cbf72cfb171e1330916ac06ebd8fb02f86
rafaqz <rafaelschouten@gmail.com>
2024-01-09 00:28:59 +0100

    fix boundscheck

--- test/stencils.jl
+++ test/stencils.jl
@@ -105,8 +105,10 @@ end
     @test isbits(h1)
     @test radius(h1) == 2
     @test length(h1) == 8
-    res1 = stencil(StencilArray(win, h1), (3, 3)) 
-    @test neighbors(res1) == SVector(0, 0, 0, 0, 1, 0, 0, 1)
+    A = StencilArray(win, h1)
+    res1 = stencil(A, (3, 3)) 
+    indices(h1, (3, 3)) 
+    @test @inferred neighbors(res1) == SVector(0, 0, 0, 0, 1, 0, 0, 1)
     @test sum(res1) == 2
 
     win3 = cat(win, win, win; dims=3)
@@ -115,7 +117,7 @@ end
     @test length(h3) == 2 * 2 * 1
     A = StencilArray(win3, h3)
     res3 = stencil(A, (2, 2, 1)) 
-    @test neighbors(res3) == SVector(1, 0, 0, 1)
+    @test @inferred neighbors(res3) == SVector(1, 0, 0, 1)
     @test sum(res3) == 2
 end
 
