44f716dead89fab9a5072d6541d0b8002aeabf2a
Rafael Schouten <rafaelschouten@gmail.com>
2025-03-29 13:15:11 +0100

    fix size and cleanup

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,19 +2,17 @@ using Stencils
 using Aqua
 using Test
 
-if VERSION >= v"1.9.0"
-    @testset "Aqua.jl" begin
-        Aqua.test_all(
-            Stencils;
-            ambiguities=(exclude=[Base.copy!, Base.copyto!],),
-            unbound_args=true,
-            undefined_exports=true,
-            project_extras=true,
-            stale_deps=true,
-            deps_compat=true,
-            piracies=true,
-        )
-    end
+@testset "Aqua.jl" begin
+    Aqua.test_all(
+        Stencils;
+        ambiguities=(exclude=[Base.copy!, Base.copyto!],),
+        unbound_args=true,
+        undefined_exports=true,
+        project_extras=true,
+        stale_deps=true,
+        deps_compat=true,
+        piracies=true,
+    )
 end
 
 include("stencils.jl")
